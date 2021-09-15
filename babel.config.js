1cdecbdbffa3c1877ee3b70ad63130c074432aea
stonkoshkur <sergiy.tonkoshkur@gmail.com>
2021-09-15 11:00:49 +0300

    feat(core): add modules aliases and pre-commit hooks

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.es',
+          '.es6',
+          '.mjs',
+          '.json',
+        ],
+        root: ['.'],
+        alias: {
+          src: './src/',
+          '@src': './src/',
+        },
+      },
+    ],
+    ['babel-plugin-root-import'],
+  ],
 };
